edc50e9e309114e321e7dcf50a07287cc6811bf8
akshat_1074 <valaniakshat1074@gmail.com>
2025-03-28 23:36:50 +0530

    Day-2

--- src/components/users/UserAcitivityHeatmap.jsx
+++ src/components/users/UserAcitivityHeatmap.jsx
@@ -24,7 +24,27 @@ const UserAcitivityHeatmap = () => {
 
             <h2 className="text-xl font-semibold text-gray-100 mb-4">User Activity Heatmap</h2>
             <div style={{width:"100%",height:300}}>
-                 
+                 <ResponsiveContainer>
+                     <BarChart data={userActivityData}>
+                        <CartesianGrid strokeDasharray='3 3' stroke="#374151"/>
+                        <XAxis dataKey='name' stroke="#9CA3AF" />
+                        <YAxis stroke="#9CA3AF"/>
+                      <Tooltip  
+                            contentStyle={{
+                              background:"rgba(31,41,55,0.8)",
+                              borderColor:"#4B5563"
+                            }}
+                             itemStyle={{color:"#E5E7EB"}}
+                            />
+                          <Legend />
+						<Bar dataKey='0-4' stackId='a' fill='#6366F1' />
+						<Bar dataKey='4-8' stackId='a' fill='#8B5CF6' />
+						<Bar dataKey='8-12' stackId='a' fill='#EC4899' />
+						<Bar dataKey='12-16' stackId='a' fill='#10B981' />
+						<Bar dataKey='16-20' stackId='a' fill='#F59E0B' />
+						<Bar dataKey='20-24' stackId='a' fill='#3B82F6' />
+                     </BarChart>
+                 </ResponsiveContainer>
 
             </div>
      </motion.div>
